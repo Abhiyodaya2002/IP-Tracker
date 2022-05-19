4de16efa3f1442e605b92838ff8116f22b43a54a
abhiyodaya <abhiyodayapandey@gmail.com>
2022-05-20 00:02:19 +0530

    third

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ let geo=document.getElementById('geo');
 let timezone=document.getElementById('timezone');
 let version=document.getElementById('version');
 button.addEventListener('click', ()=>{
-    fetch('http://ip-api.com/json/')
+    fetch('https://ipapi.co/json')
     .then((response) =>{return response.json()})
     .then((data)=>{
        // console.log(data);
       // alert(`Your IP Address is: ${data.query}`)
-       ip.innerHTML=`Your IP Address is: ${data.query}`;
-       version.innerHTML=`IP version is: IPV4`;
-       loc.innerHTML=`Your location is: ${data.city} ${data.regionName}, ${data.country}`;
-         geo.innerHTML=`Your Geo location is: ${data.lat}  ${data.lon}`;
+       ip.innerHTML=`Your IP Address is: ${data.ip}`;
+       version.innerHTML=`IP version is: ${data.version}`;
+       loc.innerHTML=`Your location is: ${data.city} ${data.region}, ${data.country_name}`;
+         geo.innerHTML=`Your Geo location is: ${data.latitude}  ${data.longitude}`;
          timezone.innerHTML=`Your time zone is: ${data.timezone}`;
     })
 })
